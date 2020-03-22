286785b949edfe7c4c25fb475eb834d75a45898d
Santosh Kumar Verma <verma006santosh@gmail.com>
2020-03-22 08:02:52 -0700

    Spring JDBC authentication with H2 databases

---
+++ SpringSecurityJDBC/src/test/java/com/springsecurity/SpringSecurityJDBC/SpringSecurityJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.SpringSecurityJDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
